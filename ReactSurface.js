c85117bf40d969abf219f2d4d9b7c5f8bf06cbd2
Chris Kim <cj3kim@gmail.com>
2015-03-31 00:49:54 -0700

    Initial Commit

---
+++ ReactSurface.js
@@ -0,0 +1,24 @@
+var Surface = require('famous/core/surface');
+var React = require('react');
+
+function transparentApply(fun, that, args) {
+  return fun.apply(that, Array.prototype.slice.apply(args));
+}
+
+function ReactSurface() {
+  transparentApply(Surface,this,arguments);
+}
+ReactSurface.prototype = Object.create(Surface.prototype);
+ReactSurface.prototype.constructor = ReactSurface;
+
+ReactSurface.prototype.deploy = function deploy (target) {
+  var content = this.getContent();
+
+  if (React.isValidElement(content)){
+    React.render(content,target);
+  } else {
+    throw new Error("Content is not a valid react component");
+  }
+}
+
+module.exports = ReactSurface;
